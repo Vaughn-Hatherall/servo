cdcc1382fb4d4d58c7ece93135faa4b01995b65c
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-09-30 13:11:36 -0400

    Final commit

--- main.ts
+++ main.ts
@@ -2,6 +2,14 @@
 // Created on: september30
 // 
 // This program turns a servo
+robotbit.Servo(robotbit.Servos.S1, 180)
+basic.pause(2000)
+robotbit.Servo(robotbit.Servos.S1, 90)
+basic.pause(2000)
+robotbit.Servo(robotbit.Servos.S1, 45)
+basic.pause(2000)
+robotbit.Servo(robotbit.Servos.S1, 135)
+basic.showIcon(IconNames.Yes)
 basic.forever(function () {
 	
 })
